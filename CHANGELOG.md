e0ae0ddc496b4235f57637134536926bf778d89e
nju33 <nju33.ki@gmail.com>
2017-01-16 18:48:29 +0900

    Fix lint

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
-## 0.0.0
+## 0.1.0
+- Fix to make it not dependent on diz package
+
+## 0.0.1
 - Release
--- test/test.js
+++ test/test.js
@@ -1,3 +1,3 @@
-import test from 'ava';
-
-test.todo('todo');
+// import test from 'ava';
+//
+// test.todo('todo');
